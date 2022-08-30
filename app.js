048b3ea180486f1c141029fd0a94e4578836a56c
Annika <nikzneko@gmail.com>
2022-08-29 18:15:35 -0700

    working on scanline-printing...

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+import { Color } from "./color"
+
+
+/**
+ * Draws a given scanline to the canvas
+ * @param {number} y Scanline Y-value, from top = 0
+ * @param {Array} lineBuffer Array containing color values for the scanline
+ * @param {Object} c Handle for canvas
+ */
+function drawScanline(y, lineBuffer, c) {
+  for (let x = 0; x < lineBuffer.length; x++) {
+    c.fillStyle = Color.fifteenToHex(lineBuffer[x])
+    c.fillRect(x, y, 1, 1)
+  }
+}
+
+
 const canvas = document.querySelector('canvas')
 const c = canvas?.getContext('2d')
-
 c.beginPath()
-// c?.moveTo(128, 128)
-c.fillStyle = 'purple'
-c.fillRect(128, 128, 16, 16)
-// c?.lineTo(200,200)
-// c.fill()
+
+// create dummy scanline
+let myScanline = []
+for (let x = 0; x < 384; x++) {
+  
+}
